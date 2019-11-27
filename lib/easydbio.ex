b7d12a97eaac60699166244209954703fa269b0b
gs <gs@adwirk.com>
2019-11-27 17:01:57 +0300

    mix new

---
+++ lib/easydbio.ex
@@ -0,0 +1,18 @@
+defmodule Easydbio do
+  @moduledoc """
+  Documentation for Easydbio.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Easydbio.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
